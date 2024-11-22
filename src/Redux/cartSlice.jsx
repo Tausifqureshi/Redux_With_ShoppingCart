065186dff7156960ef61a784561d16520b73cf9f
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-22 10:18:25 +0530

    Shopping Cart

---
+++ src/Redux/cartSlice.jsx
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+const cartSlice = createSlice ({
+    name : "cart",
+    initialState : [],
+
+    reducers :{
+        addItem : (state, action) =>{},
+        increment : (state, action) =>{},
+        decrement : (state, action) =>{},
+        removeItem : (state, action) =>{},
+    }
+})
+console.log(cartSlice.actions);
+export const {addItem,increment,decrement,removeItem} = cartSlice.actions
+
+export default cartSlice;
