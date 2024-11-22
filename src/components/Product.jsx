2f47611ec232beef3f0c12414805312a241874cd
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-22 15:32:54 +0530

    Redux With Shopping Cart

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { useCart } from '../contexts/CartProvider'
 import styles from "./Product.module.css";
 import {toast} from "react-toastify";
+import { addItem } from '../Redux/cartSlice';
+import { useDispatch } from 'react-redux';
+
 function Product({id,img,  title, price}) {
 const { addItemsCart, cart} = useCart()
 //zero se kam tu is se kam products kam nhi hoge.
- //button disabled ho jaye ga zero ke baad.
+//button disabled ho jaye ga zero ke baad.
 function handleAdd (){  
   const isProductInCart = cart.some(item => item.id === id);
   //check ek products agar add hai fir se add nhi hoga. cart ko ham useCart se lere hai Destructuring se.
@@ -15,7 +18,7 @@ function handleAdd (){
    });
     return;
   }
-
+  
   const newCartItem ={
     id: id,
     price: price,
@@ -23,6 +26,8 @@ function handleAdd (){
     img: img,   
     quantity: 1,
   }
+
+ console.log("products add", newCartItem)
  addItemsCart(newCartItem);
 
  toast.success("Product added", {
